fa654ea29a1f2caab6c2d833060cd9a88eb0d1d6
blasian <mikespearman.e@gmail.com>
2014-12-14 23:35:34 -0800

    removing maps.js

--- soundmap.js
+++ soundmap.js
@@ -1,109 +1,109 @@
-var map;
-var marker;
-var geocoder;
-var locations = [];
-var offset = 10;
+    var map;
+    var marker;
+    var geocoder;
+    var locations = [];
+    var offset = 10;
 
-SC.initialize({
-	client_id: "64a4fa9fc8c9797be3c6dc27067c131c",
-});
-
-function initialize() {
-    // Populating locations array
-    findLocations();
-    geocoder = new google.maps.Geocoder();
-    var mapOptions = {
-        center: { lat: 0, lng: 0},
-        zoom: 3,
-        disableDefaultUI: true,
-        zoomControl: false,
-        scaleControl: false,
-        scrollwheel: false,
-        disableDoubleClickZoom: true
-    };
-
-    // Map styling
-    var styles = [
-    {
-        stylers: [
-            { saturation: -100 }
-        ]
-    },{
-        featureType: "all",
-        elementType: "labels",
-        stylers: [
-            { visibility: "off" }
-        ]
-    },{
-        featureType: "road",
-        elementType: "all",
-        stylers: [
-            { visibility: "off" }
-        ]
-    },{
-        featureType: "landscape.natural.terrain",
-        stylers: [
-            { visibility: "off" }
-        ]
-    },{
-        featureType: "landscape.natural.landcover",
-        stylers: [
-            { visibility: "off" }
-        ]
-    },{
-        featureType: "country",
-        elementType: "geometry.stroke",
-        stylers: [
-            { visibility: "off" }
-        ]
-    },{
-        featureType: "administrative",
-        elementType: "geometry",
-        stylers: [
-          { visibility: "off" }
-        ]
-      }
-    ];
-
-    // Creating map
-    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-    map.setOptions({styles: styles});
-}
-
-// Create locations array
-function findLocations() {
-    SC.get("/tracks", { limit: 10, offset: offset }, function(tracks){
-        offset += 10;
-        for (var i = 0; i < tracks.length; i++) {
-            SC.get("/users/" + tracks[i].user_id, function(user){
-                if (user.country) { 
-                    locations.push(user.country);
-                }
-            });
-        }
+    SC.initialize({
+    	client_id: "64a4fa9fc8c9797be3c6dc27067c131c",
     });
-}
 
-// Add next location to map
-function locate() {
-    console.log(locations);
-    if (locations.length > 0) {
-        gcode(locations.pop());
-    } else {
+    function initialize() {
+        // Populating locations array
         findLocations();
-        locate();
-    }
-}
+        geocoder = new google.maps.Geocoder();
+        var mapOptions = {
+            center: { lat: 0, lng: 0},
+            zoom: 3,
+            disableDefaultUI: true,
+            zoomControl: false,
+            scaleControl: false,
+            scrollwheel: false,
+            disableDoubleClickZoom: true
+        };
 
-// Mark location onto map
-function gcode(country) {
-	geocoder.geocode( { 'address': country }, function(results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-            map.setCenter(results[0].geometry.location);
-            var marker = new google.maps.Marker({
-                map: map,
-                position: results[0].geometry.location
-            });
+        // Map styling
+        var styles = [
+        {
+            stylers: [
+                { saturation: -100 }
+            ]
+        },{
+            featureType: "all",
+            elementType: "labels",
+            stylers: [
+                { visibility: "off" }
+            ]
+        },{
+            featureType: "road",
+            elementType: "all",
+            stylers: [
+                { visibility: "off" }
+            ]
+        },{
+            featureType: "landscape.natural.terrain",
+            stylers: [
+                { visibility: "off" }
+            ]
+        },{
+            featureType: "landscape.natural.landcover",
+            stylers: [
+                { visibility: "off" }
+            ]
+        },{
+            featureType: "country",
+            elementType: "geometry.stroke",
+            stylers: [
+                { visibility: "off" }
+            ]
+        },{
+            featureType: "administrative",
+            elementType: "geometry",
+            stylers: [
+              { visibility: "off" }
+            ]
+          }
+        ];
+
+        // Creating map
+        map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+        map.setOptions({styles: styles});
+    }
+
+    // Create locations array
+    function findLocations() {
+        SC.get("/tracks", { limit: 10, offset: offset }, function(tracks){
+            offset += 10;
+            for (var i = 0; i < tracks.length; i++) {
+                SC.get("/users/" + tracks[i].user_id, function(user){
+                    if (user.country) { 
+                        locations.push(user.country);
+                    }
+                });
+            }
+        });
+    }
+
+    // Add next location to map
+    function locate() {
+        console.log(locations);
+        if (locations.length > 0) {
+            gcode(locations.pop());
+        } else {
+            findLocations();
+            locate();
         }
-	});
-}
+    }
+
+    // Mark location onto map
+    function gcode(country) {
+    	geocoder.geocode( { 'address': country }, function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                map.setCenter(results[0].geometry.location);
+                var marker = new google.maps.Marker({
+                    map: map,
+                    position: results[0].geometry.location
+                });
+            }
+    	});
+    }
